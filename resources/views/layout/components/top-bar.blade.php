3a4c10afbd51fa9cc236c7c63c686bbb149f7e6f
viralS-huylt <huylt@viralsoft.vn>
2020-11-11 21:18:31 +0700

    fix zoom

--- resources/views/layout/components/top-bar.blade.php
+++ resources/views/layout/components/top-bar.blade.php
@@ -1,7 +1,7 @@
 <!-- BEGIN: Top Bar -->
-<div class="top-bar">
+<div class="top-bar" style="position: relative">
     <!-- BEGIN: Breadcrumb -->
-    <div class="-intro-x breadcrumb  hidden sm:flex" style="margin-right: 60%">
+    <div class="-intro-x breadcrumb  hidden sm:flex">
         <a href="" class="">Application</a>
         <i data-feather="chevron-right" class="breadcrumb__icon"></i>
         <a href="" class="breadcrumb--active">Dashboard</a>
@@ -9,8 +9,8 @@
     <!-- END: Breadcrumb -->
     <!-- BEGIN: Search -->
 
-
-    <div class="intro-x relative mr-3 sm:mr-6">
+    <div class="grid grid-cols-2 gap-4 user">
+    <div class="intro-x relative mr-3 sm:mr-6" >
         <div class="search hidden sm:block">
             <input type="text" class="search__input input placeholder-theme-13" placeholder="Search...">
             <i data-feather="search" class="search__icon dark:text-gray-300"></i>
@@ -70,36 +70,38 @@
 
     <!-- END: Search -->
     <!-- BEGIN: Notifications -->
-    <div class="intro-x dropdown mr-auto sm:mr-6">
-        <div class="dropdown-toggle notification notification--bullet cursor-pointer">
-            <i data-feather="bell" class="notification__icon dark:text-gray-300"></i>
-        </div>
-        <div class="notification-content pt-2 dropdown-box">
-            <div class="notification-content__box dropdown-box__content box dark:bg-dark-6">
-                <div class="notification-content__title">Notifications</div>
-                @foreach (array_slice($fakers, 0, 5) as $key => $faker)
-                    <div class="cursor-pointer relative flex items-center {{ $key ? 'mt-5' : '' }}">
-                        <div class="w-12 h-12 flex-none image-fit mr-1">
-                            <img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="{{ asset('dist/images/' . $faker['photos'][0]) }}">
-                            <div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-                        </div>
-                        <div class="ml-2 overflow-hidden">
-                            <div class="flex items-center">
-                                <a href="javascript:;" class="font-medium truncate mr-5">{{ $faker['users'][0]['name'] }}</a>
-                                <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">{{ $faker['times'][0] }}</div>
-                            </div>
-                            <div class="w-full truncate text-gray-600">{{ $faker['news'][0]['short_content'] }}</div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
-{{--    <!-- END: Notifications -->--}}
-{{--    <!-- BEGIN: Account Menu -->--}}
+
+{{--    <div class="intro-x dropdown mr-auto sm:mr-6">--}}
+{{--        <div class="dropdown-toggle notification notification--bullet cursor-pointer">--}}
+{{--            <i data-feather="bell" class="notification__icon dark:text-gray-300"></i>--}}
+{{--        </div>--}}
+{{--        <div class="notification-content pt-2 dropdown-box">--}}
+{{--            <div class="notification-content__box dropdown-box__content box dark:bg-dark-6">--}}
+{{--                <div class="notification-content__title">Notifications</div>--}}
+{{--                @foreach (array_slice($fakers, 0, 5) as $key => $faker)--}}
+{{--                    <div class="cursor-pointer relative flex items-center {{ $key ? 'mt-5' : '' }}">--}}
+{{--                        <div class="w-12 h-12 flex-none image-fit mr-1">--}}
+{{--                            <img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="{{ asset('dist/images/' . $faker['photos'][0]) }}">--}}
+{{--                            <div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>--}}
+{{--                        </div>--}}
+{{--                        <div class="ml-2 overflow-hidden">--}}
+{{--                            <div class="flex items-center">--}}
+{{--                                <a href="javascript:;" class="font-medium truncate mr-5">{{ $faker['users'][0]['name'] }}</a>--}}
+{{--                                <div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">{{ $faker['times'][0] }}</div>--}}
+{{--                            </div>--}}
+{{--                            <div class="w-full truncate text-gray-600">{{ $faker['news'][0]['short_content'] }}</div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                @endforeach--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+
+    <!-- END: Notifications -->
+    <!-- BEGIN: Account Menu -->
     <div class="intro-x dropdown w-8 h-8">
-        <div class="dropdown-toggle w-8 h-8 rounded-full overflow-hidden shadow-lg image-fit zoom-in" style="width: 8rem; text-align: center">
-{{--            <img alt="Midone Tailwind HTML Admin Template" src="{{ asset('dist/images/' . $fakers[9]['photos'][0]) }}">--}}
+        <div class="dropdown-toggle w-8 h-8 rounded-full overflow-hidden shadow-lg image-fit zoom-in" style="width: 8rem; text-align: center; padding-top: 3px">
             <p>{{ \Illuminate\Support\Facades\Auth::user() ? \Illuminate\Support\Facades\Auth::user()->name : ""}}</p>
         </div>
         <div class="dropdown-box w-56">
@@ -129,6 +131,7 @@
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <!-- END: Account Menu -->
 </div>
