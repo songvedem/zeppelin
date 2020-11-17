9b83ed37364f0b9b3587ed15f6aaca70f7d5defc
viralS-huylt <huylt@viralsoft.vn>
2020-11-17 22:22:57 +0700

    huy edit

--- resources/views/pages/detection-threshold.blade.php
+++ resources/views/pages/detection-threshold.blade.php
@@ -51,6 +51,9 @@
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Update
                 </button>
+                <p>
+                    Last update: {{$detectionThreshold->updated_at}}
+                </p>
             </div>
         </form>
 @endsection
